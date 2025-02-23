7f432532a53e26f83bd42c3c5220bb9846cee820
Igor Contri <igorcontri@gmail.com>
2025-02-23 13:21:51 -0300

    lista 1, ex 7 ao 20

--- 01_exercicios/lista_01/exer17envio.php
+++ 01_exercicios/lista_01/exer17envio.php
@@ -7,18 +7,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Exercicio 2</h1>
+    <h1>Exercicio 17</h1>
 
-    <form method="post" action="exer02resposta.php">  
+    <form method="post" action="exer17resposta.php" class="border w-50 p-3">  
     <div class="mb-3">
-        <label for="valor1" class="form-label">Informar primeiro valor</label>
+        <label for="valor1" class="form-label">Informar Capital</label>
         <input type="number" id="valor1" name="valor1" class="form-control" required="">
     </div>
-
     <div class="mb-3">
-        <label for="valor2" class="form-label">Informar segundo valor</label>
+        <label for="valor2" class="form-label">Informar Taxa de Juros</label>
         <input type="number" id="valor2" name="valor2" class="form-control" required="">
     </div>
+    <div class="mb-3">
+        <label for="valor3" class="form-label">Informar Periodo</label>
+        <input type="number" id="valor3" name="valor3" class="form-control" required="">
+    </div>
+    
     <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- 01_exercicios/lista_01/exer18resposta.php
+++ 01_exercicios/lista_01/exer18resposta.php
@@ -7,18 +7,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Reposta do Exercicio 02</h1>
+    <h1>Reposta do Exercicio 18</h1>
     <?php 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            try {
+          try {
             $valor1 = $_POST['valor1'];
             $valor2 = $_POST['valor2'];
-            $sub = $valor1 - $valor2;
+            $valor3 = $_POST['valor3'];
+            $montante = ($valor1 * (1 + ($valor2 / 100)) ** $valor3);
 
-            echo "O valor da subtração é: $sub";
-            } catch(Exception $e){
-                echo $e->getMessage();
-            }
+            echo "O montante será de $montante";
+          } catch(Exception $e){
+            echo $e->getMessage();
+          }
 
         } 
     ?>
