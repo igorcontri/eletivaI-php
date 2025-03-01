9ceef1d482bb1eeffce10aa13740a3cb8a246cbb
Igor Contri <igorcontri@gmail.com>
2025-03-01 13:49:15 -0300

    lista 03: exercicio 1

---
+++ 01_exercicios/lista_03/exer01envio.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+      
+      <h1>Exercicio 01</h1>
+
+     <form method="post" action="exer01resposta.php">  
+      <div class="mb-3">
+          <label for="valor1" class="form-label">Informar primeiro valor</label>
+          <input type="number" id="valor1" name="valor1" class="form-control" required="">
+      </div>
+  
+      <div class="mb-3">
+          <label for="valor2" class="form-label">Informar segundo valor</label>
+          <input type="number" id="valor2" name="valor2" class="form-control" required="">
+      </div>
+      <div class="mb-3">
+          <label for="valor3" class="form-label">Informar segundo valor</label>
+          <input type="number" id="valor3" name="valor3" class="form-control" required="">
+      </div>
+      <div class="mb-3">
+          <label for="valor4" class="form-label">Informar segundo valor</label>
+          <input type="number" id="valor4" name="valor4" class="form-control" required="">
+      </div>
+      <div class="mb-3">
+          <label for="valor5" class="form-label">Informar segundo valor</label>
+          <input type="number" id="valor5" name="valor5" class="form-control" required="">
+      </div>
+      <div class="mb-3">
+          <label for="valor6" class="form-label">Informar segundo valor</label>
+          <input type="number" id="valor6" name="valor6" class="form-control" required="">
+      </div>
+      <div class="mb-3">
+          <label for="valor7" class="form-label">Informar segundo valor</label>
+          <input type="number" id="valor7" name="valor7" class="form-control" required="">
+      </div>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+            
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ 01_exercicios/lista_03/exer01resposta.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resposta</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Reposta do Exercicio 01</h1>
+
+    <?php 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          try {
+
+            for ($i=1; $i <= 7; $i++){
+              $valor = $_POST["valor{$i}"];
+
+              if ($i == 1){
+                $menor_valor = $valor;
+                $maior_valor = $valor;
+              }
+
+              if ($valor <= $menor_valor){
+                $menor_valor = $valor;
+                $pos_menor = $i;
+              }
+              
+              if ($valor >= $maior_valor){
+                $maior_valor = $valor;
+                $pos_maior = $i;
+              }
+            }
+            
+            echo "O maior valor é: $maior_valor na posição: $pos_maior <br/>"; //perdão
+            echo "O menor valor é: $menor_valor na posição: $pos_menor";
+
+          } catch(Exception $e){
+              echo $e->getMessage();
+          }
+        } 
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
