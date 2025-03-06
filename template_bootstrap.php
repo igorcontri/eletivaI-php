f1b770bec0d89ea3b4775c6b640966667e6c9ff1
Igor Contri <igorcontri@gmail.com>
2025-03-06 19:14:35 -0300

    atualizando template.php

--- template_bootstrap.php
+++ template_bootstrap.php
@@ -3,21 +3,26 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Exemplo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
-  <h1>Title</h1>
+  <body class="d-flex flex-column justify-content-center align-items-center vh-100">
 
-    <?php 
-      if ("teste") {
-        try {
+    <div class="border w-50 p-3">
+      <h1>Exemplo</h1>
 
-        } catch(Exception $e){
-          echo $e->getMessage();
-        }
-      } 
-    ?>
+      <?php 
+          if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            try {
+
+
+            } catch(Exception $e){
+                echo $e->getMessage();
+            }
+          } 
+      ?>
+    </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
