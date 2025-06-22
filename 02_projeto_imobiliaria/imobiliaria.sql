770af89186e1ebe9f792aa38cc0f0d63c9131fe1
Igor Contri <igorcontri@gmail.com>
2025-06-22 16:42:46 -0300

    fix SQL e Index

--- 02_projeto_imobiliaria/imobiliaria.sql
+++ 02_projeto_imobiliaria/imobiliaria.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Abr-2025 às 00:13
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.2.0
+-- Generation Time: Jun 22, 2025 at 08:43 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,68 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `projetophp`
+-- Database: `imobiliaria`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuarios`
+-- Table structure for table `contratos_locacao`
+--
+
+CREATE TABLE `contratos_locacao` (
+  `id` int(11) NOT NULL,
+  `data_inicio` date NOT NULL,
+  `data_fim` date NOT NULL,
+  `id_proprietario` int(11) NOT NULL,
+  `id_locatario` int(11) NOT NULL,
+  `id_imovel` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imoveis`
+--
+
+CREATE TABLE `imoveis` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `endereco` varchar(45) NOT NULL,
+  `valor` varchar(45) NOT NULL,
+  `area_total` decimal(6,1) DEFAULT NULL,
+  `area_util` decimal(5,1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locatarios`
+--
+
+CREATE TABLE `locatarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `cpf_cnpj_locatario` varchar(45) NOT NULL,
+  `imoveis_locados` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proprietarios`
+--
+
+CREATE TABLE `proprietarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `cpf_cnpj_proprietario` varchar(45) NOT NULL,
+  `imoveis_adquiridos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -32,55 +87,103 @@ CREATE TABLE `usuarios` (
   `nome` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
   `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Índices para tabelas despejadas
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'igor', 'igor@adm', '$2y$10$9kDmchttgk/yNmtkpbHExOKWcOyFYyBpQV4QaCCHRn0nud3H.W25e');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `usuarios`
+-- Indexes for table `contratos_locacao`
+--
+ALTER TABLE `contratos_locacao`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idcontratos_locacao_UNIQUE` (`id`),
+  ADD KEY `fk_contratos_locacao_proprietarios_idx` (`id_proprietario`),
+  ADD KEY `fk_contratos_locacao_locatarios1_idx` (`id_locatario`),
+  ADD KEY `fk_contratos_locacao_imoveis1_idx` (`id_imovel`);
+
+--
+-- Indexes for table `imoveis`
+--
+ALTER TABLE `imoveis`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_imovel_UNIQUE` (`id`);
+
+--
+-- Indexes for table `locatarios`
+--
+ALTER TABLE `locatarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_proprietarios_UNIQUE` (`id`);
+
+--
+-- Indexes for table `proprietarios`
+--
+ALTER TABLE `proprietarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_proprietarios_UNIQUE` (`id`);
+
+--
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `usuarios`
+-- AUTO_INCREMENT for table `contratos_locacao`
+--
+ALTER TABLE `contratos_locacao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `imoveis`
+--
+ALTER TABLE `imoveis`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `locatarios`
+--
+ALTER TABLE `locatarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `proprietarios`
+--
+ALTER TABLE `proprietarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contratos_locacao`
+--
+ALTER TABLE `contratos_locacao`
+  ADD CONSTRAINT `fk_contratos_locacao_imoveis1` FOREIGN KEY (`id_imovel`) REFERENCES `imoveis` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_contratos_locacao_locatarios1` FOREIGN KEY (`id_locatario`) REFERENCES `locatarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_contratos_locacao_proprietarios` FOREIGN KEY (`id_proprietario`) REFERENCES `proprietarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE IF NOT EXISTS `categoria` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(255) NOT NULL,
-  `descricao` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `bancophp`.`produto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `produto` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(255) NULL,
-  `descricao` VARCHAR(255) NULL,
-  `preco` DECIMAL(8,2) NULL,
-  `categoria_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_produto_categoria_idx` (`categoria_id` ASC),
-  CONSTRAINT `fk_produto_categoria`
-    FOREIGN KEY (`categoria_id`)
-    REFERENCES `categoria` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
